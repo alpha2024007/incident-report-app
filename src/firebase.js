48a7f3ad19986fde84ba818e2f1574909a58dffa
New Author <alhajialphatejancole@gmail.com>
2024-05-26 16:51:19 +0300

    added firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsudc7jWdLLjPcGj4rTlPDzUrDqM-3YNI",
+  authDomain: "sos-app-402c3.firebaseapp.com",
+  projectId: "sos-app-402c3",
+  storageBucket: "sos-app-402c3.appspot.com",
+  messagingSenderId: "824711504343",
+  appId: "1:824711504343:web:bebabac48156ee25e86b92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
